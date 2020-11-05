334c26b6908ca34eb5c6bde7a7a92fd013c99cc2
lorenzo galli <lrnzgll@gmail.com>
2020-11-05 18:51:37 +0000

    job css]

--- app/decorators/job_decorator.rb
+++ app/decorators/job_decorator.rb
@@ -16,14 +16,4 @@ class JobDecorator < Draper::Decorator
   def last_updated_time
     object.updated_at.strftime("%H:%M")
   end
-
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
 end
