f1e1d2f6d39e34754607031da86d7666b81cc7ff
lorenzo galli <lrnzgll@gmail.com>
2020-11-11 12:36:38 +0000

    wip

---
+++ db/migrate/20201111123152_create_periods.rb
@@ -0,0 +1,11 @@
+class CreatePeriods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :periods do |t|
+      t.references :job, foreign_key: true
+      t.timestamp :start_time, null: false
+      t.timestamp :end_time
+
+      t.timestamps
+    end
+  end
+end
